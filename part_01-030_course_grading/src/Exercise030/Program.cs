2a59534be605f6e2022e322468de5fb8b391e1ef
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 12:58:10 +0300

    30 Done

--- part_01-030_course_grading/src/Exercise030/Program.cs
+++ part_01-030_course_grading/src/Exercise030/Program.cs
@@ -5,6 +5,47 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine($"Give your percent [0 - 100]:");
+
+      int num = int.Parse(Console.ReadLine());
+
+      if ( num < 0)
+      {
+        Console.WriteLine("Impossible");
+      }
+      else if (num <= 49)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if(num <= 59)
+      {
+        Console.WriteLine("Grade: 1");
+      
+      }
+      else if (num <= 69)
+      {
+        Console.WriteLine("Grade: 2");
+
+      }
+      else if (num <= 79)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+
+      else if (num <= 89)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (num <= 100)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      if (num > 100)
+      {
+        Console.WriteLine("Outstanding!");
+      }
+
+      
     }
   }
 }
