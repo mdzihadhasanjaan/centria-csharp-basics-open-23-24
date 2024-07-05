3ecc7bd6e172b80eb5f6b69852098d5a370cf753
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 18:55:40 +0300

    31 Done

--- part_01-031_even_or_odd/src/Exercise031/Program.cs
+++ part_01-031_even_or_odd/src/Exercise031/Program.cs
@@ -5,6 +5,17 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a number:");
+      int x = int.Parse(Console.ReadLine());
+      if (x % 2 == 0)
+      {
+        Console.WriteLine("It is even.");
+      
+      }
+      else 
+    {
+      Console.WriteLine("It is odd.");
+    }
     }
   }
 }
--- part_01-031_even_or_odd/src/Exercise031/obj/Debug/net6.0/Exercise.AssemblyInfo.cs
+++ part_01-031_even_or_odd/src/Exercise031/obj/Debug/net6.0/Exercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a59534be605f6e2022e322468de5fb8b391e1ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
