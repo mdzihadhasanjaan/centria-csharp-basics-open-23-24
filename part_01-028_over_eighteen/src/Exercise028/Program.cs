f7d418598550172e71ac2c3656bf030131c56db4
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 11:31:56 +0300

    28 Done

--- part_01-028_over_eighteen/src/Exercise028/Program.cs
+++ part_01-028_over_eighteen/src/Exercise028/Program.cs
@@ -5,6 +5,16 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("How old are you?");
+      int x = int.Parse(Console.ReadLine());
+      if (x >= 18)
+      {
+        Console.WriteLine("You're an adult!");
+      }
+      else
+      {
+        Console.WriteLine("You're under age!");
+      }
     }
   }
 }
