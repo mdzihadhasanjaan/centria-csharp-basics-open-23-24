9eeb29df7cce2a5a5cfbfbf3622173f9b7b444b4
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 11:13:02 +0300

    23 Done

--- part_01-023_tiny_calculator/src/Exercise023/Program.cs
+++ part_01-023_tiny_calculator/src/Exercise023/Program.cs
@@ -5,6 +5,18 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      int x = int.Parse(Console.ReadLine());
+      Console.WriteLine("Give the second number!");
+      int y = int.Parse(Console.ReadLine());
+      int sum = x + y;
+      int sub = x - y;
+      int mul = x * y;
+      double div = (double)x / y;
+      Console.WriteLine($"{x} + {y} = {sum}");
+      Console.WriteLine($"{x} - {y} = {sub}");
+      Console.WriteLine($"{x} * {y} = {mul}");
+      Console.WriteLine($"{x} / {y} = {div}");
     }
   }
 }
