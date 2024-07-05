0022084d461b7c1750c71ad004272be9691b4762
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 19:29:17 +0300

    34 Done

--- part_01-034_continue/src/Exercise034/Program.cs
+++ part_01-034_continue/src/Exercise034/Program.cs
@@ -5,6 +5,16 @@
   {
     public static void Main(string[] args)
     {
+      while (true)
+      {
+        Console.WriteLine("Do you want to continue?");
+        string userInput = Console.ReadLine();
+        if (userInput == "no")
+        {
+          break;
+        }
+      }
+
     }
   }
 }
