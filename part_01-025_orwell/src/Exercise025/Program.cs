0da3f6adbaebd63d8140df195536269c3504cfe2
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 11:19:20 +0300

    25 Done

--- part_01-025_orwell/src/Exercise025/Program.cs
+++ part_01-025_orwell/src/Exercise025/Program.cs
@@ -5,6 +5,12 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a number:");
+      int x = int.Parse(Console.ReadLine());
+      if (x == 1984)
+      {
+        Console.WriteLine("Orwell");
+      }
     }
   }
 }
--- part_01-025_orwell/test/Exercise025Test/obj/Debug/net6.0/Exercise025Tests.AssemblyInfo.cs
+++ part_01-025_orwell/test/Exercise025Test/obj/Debug/net6.0/Exercise025Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise025Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343d569f4ad5c495b19fd9d605263221560cae43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise025Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise025Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
