043f6af6c7e813f88242cc3c9b608baa1cc94017
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 11:28:50 +0300

    27 Done

--- part_01-027_stay_positive/src/Exercise027/Program.cs
+++ part_01-027_stay_positive/src/Exercise027/Program.cs
@@ -5,6 +5,15 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a number:");
+      int x = int.Parse(Console.ReadLine());
+      if (x > 0)
+      {
+        Console.WriteLine("It is positive");
+      }
+      else {
+        Console.WriteLine("It is not positive");
+      }
     }
   }
 }
