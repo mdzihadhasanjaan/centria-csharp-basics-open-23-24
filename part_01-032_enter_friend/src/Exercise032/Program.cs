6e63eb75d9364b53ed45309717b8c556aeaa0a4a
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-05 19:00:24 +0300

    32 Done

--- part_01-032_enter_friend/src/Exercise032/Program.cs
+++ part_01-032_enter_friend/src/Exercise032/Program.cs
@@ -5,6 +5,15 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Speak, friend, and enter!");
+      string userInput = Console.ReadLine();
+      if (userInput == "Mellon")
+    {
+      Console.WriteLine("Welcome, friend");
+    }
+    else{
+      Console.WriteLine("They've got a cave troll!");
+    }
     }
   }
 }
