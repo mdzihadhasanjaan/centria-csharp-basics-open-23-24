d2c96efc81408892639c5107aa307862d9908a65
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-08 10:33:01 +0300

    37 Done

--- part_01-037_counting_numbers/src/Exercise037/Program.cs
+++ part_01-037_counting_numbers/src/Exercise037/Program.cs
@@ -5,7 +5,20 @@
   {
     public static void Main(string[] args)
     {
+      int count = 0;
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string userInput = Console.ReadLine();
+        int num = int.Parse(userInput);
+        if (num == 0)
+        {
+          Console.WriteLine($"Total amount of numbers: {count}");
+          break;
+        }
+        count++;
+      }
       
-    }
   }
+}
 }
